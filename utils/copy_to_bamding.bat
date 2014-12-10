f38f7fffce4f6ca0a012548954ca9582e3efef05
sethalicious <sethalicious@gmail.com>
2014-12-09 20:05:43 -0800

    Add script for copying files to localhost wp app

---
+++ utils/copy_to_bamding.bat
@@ -0,0 +1,4 @@
+xcopy /eiy c:\xampp\htdocs\bamding\wpmain\wp-content\tardis\* c:\xampp\apps\wordpress\htdocs\wp-content\tardis\
+xcopy /eiy c:\xampp\htdocs\bamding\wpmain\wp-content\js\* c:\xampp\apps\wordpress\htdocs\wp-content\js\
+xcopy /eiy c:\xampp\htdocs\bamding\wpmain\wp-content\themes\makechild\* c:\xampp\apps\wordpress\htdocs\wp-content\themes\make-child
+
